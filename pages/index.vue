a96aa66c7aa80f80e36997398bc1e1e3c7bb25a0
Michiel Koning <mail@michielkoning.nl>
2019-11-12 06:49:18 +0100

    search

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    home
-    <ul>
+    <input v-model="search" type="search" @keyup="searchFor">
+    {{ search }}
+    <ul v-if="toys.length">
       <li v-for="toy in toys" :key="toy.title">
         {{ toy.title }} - {{ toy.category }}
       </li>
@@ -16,6 +17,17 @@ export default {
     const toys = await axios.get('http://localhost:3000/toys.json')
     return {
       toys: toys.data
+    }
+  },
+  data () {
+    return {
+      search: 'a'
+    }
+  },
+  methods: {
+    searchFor () {
+      window.console.log('aa')
+      this.toys = []
     }
   },
   head () {
